a7cad954184418c88c025228c96a6d8defa40501
Daniel Irwin <daniel.irwin@enernoc.com>
2017-10-30 11:29:57 -0400

    turn to es6 class! and cleanup a little, add ability to track promises

--- index.js
+++ index.js
@@ -3,102 +3,124 @@
  */
 
 'use strict';
-module.exports = (function(){
-
-    var stats = require('stats-lite');
-
-    return function(meterEndCallback, maxMeters) {
-
-        const MAX_METERS = maxMeters || 10000;
-
-        var savedMetricData = {};
-
-        function meter(name) {
-            var start = process.hrtime();
-            return {
-                end: function () {
-                    var end = process.hrtime(start);
-                    calcMeter({
-                        name: name,
-                        start: start,
-                        end: ((end[0] * 1e9 + end[1]))// nanoseconds if you want millis * 1e-6
-                    });
-                }
-            };
-        }
-
-        function calcMetrics() {
-            var meterStats = {};
-            Object.keys(savedMetricData).forEach(function (key) {
-                var currentMeter = savedMetricData[key];
-                var datum = currentMeter.datum;
-                if (!currentMeter.stats) {
-                    currentMeter.stats = {};
-                }
-                meterStats[currentMeter.name] = {
-                    sum: stats.sum(datum),
-                    mean: stats.mean(datum),
-                    median: stats.median(datum),
-                    mode: stats.mode(datum),
-                    variance: stats.variance(datum),
-                    std_dev: stats.stdev(datum),
-                    p50: stats.percentile(datum, 0.50),
-                    p75: stats.percentile(datum, 0.75),
-                    p95: stats.percentile(datum, 0.95),
-                    p98: stats.percentile(datum, 0.98),
-                    p99: stats.percentile(datum, 0.99),
-                    p999: stats.percentile(datum, 0.999),
-                    min: currentMeter.min,
-                    max: currentMeter.max,
-                    count: currentMeter.count
-                };
-            });
-            return meterStats;
-        }
-
-        function calcMeter(meter) {
-            var atMeter = savedMetricData[meter.name];
-
-            if(!atMeter){
-                atMeter = {
-                  min : 100000000,
-                  max : -100000000,
-                  datum : [],
-                  count : 0
-                };
-            }
-            else if(atMeter.datum && atMeter.datum.length > MAX_METERS) {
-                atMeter.datum.shift();
-            }
-
-            atMeter.datum.push(meter.end);
-
-            savedMetricData[meter.name] = {
-                min: (atMeter.min > meter.end) ? meter.end : atMeter.min,
-                max: (atMeter.max < meter.end) ? meter.end : atMeter.max,
-                count: ++atMeter.count,
-                datum: atMeter.datum,
-                stats: atMeter.stats
-            };
+const stats = require('stats-lite');
+module.exports = class MeterFactory {
 
 
-            if(typeof meterEndCallback === 'function'){
-                meterEndCallback({
-                    name : meter.name,
-                    diff : meter.end,
-                    min : savedMetricData[meter.name].min,
-                    max : savedMetricData[meter.name].max
+    /**
+     *
+     * @param meterEndCallback - callback that gets hit on each meter end()
+     * @param maxMeters - max number of points a meter can collect before it throws away FIFO
+     * @param resolutionMultiplier - multiply by time values (values cononically in nanoseconds,
+     *      i.e. if you want milliseconds 1e-6 )
+     */
+    constructor(meterEndCallback, maxMeters, resolutionMultiplier) {
+
+        this.meterEndCallback = meterEndCallback;
+
+        this.MAX_METERS = maxMeters || 10000;
+
+        this.savedMetricData = {};
+
+        this.resolutionMultiplier = (typeof resolutionMultiplier === 'number')?resolutionMultiplier:1;
+    }
+
+    meter(name) {
+        let start = process.hrtime();
+        return {
+            end: () => {
+                let end = process.hrtime(start);
+                this.calcMeter({
+                    name: name,
+                    start: start * this.resolutionMultiplier,
+                    end: ((end[0] * 1e9 + end[1])) * this.resolutionMultiplier// nanoseconds if you want millis * 1e-6
                 });
             }
+        };
+    }
 
-            return meter.end;
+    calcMetrics() {
+        let meterStats = {};
+        Object.keys(this.savedMetricData).forEach((key) => {
+            let currentMeter = this.savedMetricData[key];
+            let datum = currentMeter.datum;
+            if (!currentMeter.stats) {
+                currentMeter.stats = {};
+            }
+            meterStats[currentMeter.name] = {
+                sum: stats.sum(datum),
+                mean: stats.mean(datum),
+                median: stats.median(datum),
+                mode: stats.mode(datum),
+                variance: stats.variance(datum),
+                std_dev: stats.stdev(datum),
+                p50: stats.percentile(datum, 0.50),
+                p75: stats.percentile(datum, 0.75),
+                p95: stats.percentile(datum, 0.95),
+                p98: stats.percentile(datum, 0.98),
+                p99: stats.percentile(datum, 0.99),
+                p999: stats.percentile(datum, 0.999),
+                min: currentMeter.min,
+                max: currentMeter.max,
+                count: currentMeter.count
+            };
+        });
+        return meterStats;
+    }
+
+    calcMeter(meter) {
+        let atMeter = this.savedMetricData[meter.name];
+
+        if (!atMeter) {
+            atMeter = {
+                min: 100000000,
+                max: -100000000,
+                datum: [],
+                count: 0
+            };
+        }
+        else if (atMeter.datum && atMeter.datum.length > this.MAX_METERS) {
+            atMeter.datum.shift();
         }
 
-        return {
-            meter: meter,
-            calcMetrics : calcMetrics,
-            getMeters : function(){return savedMetricData;}
-        };
-    };
+        atMeter.datum.push(meter.end);
 
-})();
+        this.savedMetricData[meter.name] = {
+            min: (atMeter.min > meter.end) ? meter.end : atMeter.min,
+            max: (atMeter.max < meter.end) ? meter.end : atMeter.max,
+            count: ++atMeter.count,
+            datum: atMeter.datum,
+            stats: atMeter.stats
+        };
+
+        if (typeof this.meterEndCallback === 'function') {
+            this.meterEndCallback({
+                name: meter.name,
+                diff: meter.end,
+                min: this.savedMetricData[meter.name].min,
+                max: this.savedMetricData[meter.name].max
+            });
+        }
+        return meter.end;
+    }
+
+    promiseMeter(name, functionThatResultsInPromise, argumentsAsArray, scope) {
+        if(typeof name === 'function' && typeof functionThatResultsInPromise !== 'function') {
+            functionThatResultsInPromise = name;
+            name = 'no-named-promise-meter';
+        }
+        if(typeof argumentsAsArray !== 'object' || !Array.isArray(argumentsAsArray)){
+            argumentsAsArray = [];
+        }
+        let localMeter = this.meter(name);
+        //only ends meter on resolve, reject causes meter to disolve
+        return functionThatResultsInPromise.apply(scope, argumentsAsArray).then((data) => {
+            localMeter.end();
+            return data;
+        });
+    }
+
+    getMeters() {
+        return this.savedMetricData;
+    }
+};
--- test/quickTest.js
+++ test/quickTest.js
@@ -25,7 +25,7 @@ describe('QuickTest', function(){
             }
         }
 
-        var factory = metrics(fin);
+        var factory = new metrics(fin);
 
         var myMeter = factory.meter('fancyMeter');
 
@@ -42,5 +42,67 @@ describe('QuickTest', function(){
         myMeter2.end();
 
     });
+
+    it('Seconds Test', function(done){
+
+        var maxCalls = 2;
+
+        var calls = 0;
+
+        function fin(data){
+            console.log('', data);
+
+            if(data.name === 'fancyMeter'){
+                calls++;
+
+                if(calls == maxCalls){
+                    console.log('meters', factory.getMeters());
+                    done();
+                }
+            }
+        }
+
+        var factory = new metrics(fin, 10, 1e-9);
+
+        var myMeter = factory.meter('fancyMeter');
+
+        var myMeter2 = factory.meter('fancyMeter');
+        //do some stuff
+        for(var i = 0; i < 10000; ++i){
+
+        }
+        myMeter.end();
+        //console.logs are insanely slow
+        console.log('hello');
+        myMeter.end();
+
+        myMeter2.end();
+
+    });
+
+
+    it('Seconds Test Promise', function(done){
+        this.timeout(6000);
+
+        function fin(data){
+            console.log('meter data', data);
+        }
+
+        var factory = new metrics(fin, 10, 1e-9);
+
+        function prom (ts) {
+            return new Promise((resolve)=> {
+                setTimeout(() => resolve('hello'), ts || 1000);
+            });
+        }
+
+        factory.promiseMeter('promise test meter', prom, [5000]).then( (data) => {
+            console.log('data', data);
+            if(data === 'hello'){
+                done();
+            }
+        });
+    });
+
 
 });
